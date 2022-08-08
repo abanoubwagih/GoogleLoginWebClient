d5cd2bcb750600cb57226444a0d6d2ef2d98e312
Mourad <Mourad@DESKTOP-4MP37BT>
2022-08-08 02:13:48 +0200

    crate project to login using oauth2 using google account

---
+++ src/main/java/com/appsdeveloperblog/clients/googlelogin/googleloginwebclient/GoogleLoginWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.clients.googlelogin.googleloginwebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleLoginWebClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleLoginWebClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/appsdeveloperblog/clients/googlelogin/googleloginwebclient/GoogleLoginWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.clients.googlelogin.googleloginwebclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleLoginWebClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
